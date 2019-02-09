dd73d71dbaffc41d677590ac682b500b694828c5
이우용 <wooyo250@naver.com>
2019-02-10 00:31:42 +0900

    EventCallBack

---
+++ EventCallBackTest/EventCallBackTest/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventCallBackTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SubWindow SubWin;
+        StringBuilder sb = new StringBuilder();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SubWin = new SubWindow();
+
+            // SubWin의 이벤트를 MainWindow에 있는 콜백함수에 등록
+            SubWin.MyCallBackEvent += MyCallBackFunction;
+        }
+
+        private void OpenSubWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            SubWin.Show();
+        }
+
+        public void MyCallBackFunction(DataClass data)
+        {
+            // stringbuilder 객체에 SubWin의 텍스트박스에 데이터를 추가
+            sb.Append(data.StringData);
+            sb.Append("\n");
+            textBlock.Text = sb.ToString();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SubWin.MyCallBackEvent -= MyCallBackFunction;
+        }
+
+        // 참고 : http://www.csharpstudy.com/csharp/CSharp-delegate3.aspx
+    }
+}
---
+++ EventCallBackTest/EventCallBackTest/SubWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EventCallBackTest
+{
+    /// <summary>
+    /// SubWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        private DataClass sData = null;
+        // 대리자 정의
+        public delegate void MyStringSender(DataClass data);
+
+        // SubWindow -> MainWindow로 데이터를 넘겨주기 위한 이벤트 핸들러
+        public event MyStringSender MyCallBackEvent;
+
+        public SubWindow()
+        {
+            InitializeComponent();
+
+            sData = new DataClass();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            sData.StringData = InputDataTextBox.Text;
+
+            // CallBackEvent를 호출하여 Main에 등록된 Event를 발생시킴
+            MyCallBackEvent(sData);
+        }
+
+    }
+
+    public class DataClass
+    {
+        public string StringData { get; set; }
+        
+    }
+}
