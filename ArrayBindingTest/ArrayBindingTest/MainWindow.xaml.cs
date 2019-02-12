46e7350ea2a7838b88a98f43cd7e8fba5a84d83b
이우용 <wooyo250@naver.com>
2019-02-12 22:18:02 +0900

    ArrayBindingProject

---
+++ ArrayBindingTest/ArrayBindingTest/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArrayBindingTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ArrayClassTest ArrayClassObject;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ArrayClassObject = new ArrayClassTest();
+            for (int i = 0; i < ArrayClassObject.MyArray.Length; i++)
+            {
+                ArrayClassObject.MyArray[i] = i;
+            }
+
+            for ( int si = 0; si < ArrayClassObject.MyStringArray.Length; si++)
+            {
+                ArrayClassObject.MyStringArray[si] = (si*si).ToString();
+            }
+
+            this.DataContext = ArrayClassObject;
+
+            int j = 0;
+        }
+    }
+}
---
+++ ArrayBindingTest/ArrayBindingTest/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ArrayBindingTest
+{
+    public class ArrayClassTest : INotifyPropertyChanged
+    {
+        private int[] myArray;
+        private string[] myStringArray;
+
+        public int[] MyArray
+        {
+            get { return myArray; }
+            set
+            {
+                myArray = value;
+                NotifyPropertyChanged("MyArray");
+
+            }
+        }
+
+        public string[] MyStringArray
+        {
+            get { return myStringArray; }
+            set
+            {
+                myStringArray = value;
+                NotifyPropertyChanged("MyStringArray");
+
+            }
+        }
+
+        public ArrayClassTest()
+        {
+            myArray = new int[5];
+            MyStringArray = new string[5];
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // This method is called by the Set accessor of each property.  
+        // The CallerMemberName attribute that is applied to the optional propertyName  
+        // parameter causes the property name of the caller to be substituted as an argument.  
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
