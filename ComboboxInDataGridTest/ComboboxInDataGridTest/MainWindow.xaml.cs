fd195efd5324605a49a5018482c7a76662e6ad4c
이우용 <wooyo250@naver.com>
2019-02-13 23:09:22 +0900

    Combobox In DataGrid Test

---
+++ ComboboxInDataGridTest/ComboboxInDataGridTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboboxInDataGridTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<PeopleInfo> InfoList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InfoList = new ObservableCollection<PeopleInfo>();
+
+            PeopleInfoDataGrid.ItemsSource = InfoList;
+        }
+
+        private void GeneratePeopleInfoButton_Click(object sender, RoutedEventArgs e)
+        {
+            InfoList.Add(new PeopleInfo { Name = "이", Age = 25, Address = "대전" });
+            InfoList.Add(new PeopleInfo { Name = "김", Age = 26, Address = "광주" });
+            InfoList.Add(new PeopleInfo { Name = "우", Age = 27, Address = "부산" });
+        }
+    }
+}
---
+++ ComboboxInDataGridTest/ComboboxInDataGridTest/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboboxInDataGridTest
+{
+    public class PeopleInfo
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class AddressList : List<string>
+    {
+        public AddressList()
+        {
+            this.Add("서울");
+            this.Add("광주");
+            this.Add("대전");
+            this.Add("부산");
+        }
+    }
+
+
+}
