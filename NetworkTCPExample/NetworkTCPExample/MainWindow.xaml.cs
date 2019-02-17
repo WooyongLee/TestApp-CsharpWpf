b73005e35093ec2de7a884ffd00874e3b64014fe
이우용 <wooyo250@naver.com>
2019-02-17 18:30:42 +0900

    NetworkTCPExample Commit

---
+++ NetworkTCPExample/NetworkTCPExample/MainWindow.xaml.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkTCPExample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool IsServerStop = true;
+        public ObservableCollection<TCPDataItem> DataList; // WPF DataGrid에 붙이기 위한 List
+        public TCPserverManager ServerManager;
+        public Thread ServerThread;
+
+        private int index = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ServerManager = new TCPserverManager();
+
+            ServerThread = new Thread(new ThreadStart(Listen));
+            DataList = new ObservableCollection<TCPDataItem>();
+
+            ServerManager.DataReceiveEvent += ServerManager_DataReceiveEvent;
+
+            this.ServerDataGrid.ItemsSource = DataList;
+        }
+
+
+        private void ServerStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsServerStop)
+            {
+                ServerManager.BindIP = IPtextBox.Text;
+                ServerManager.BindPort = int.Parse(PORTtextBox.Text);
+
+                ServerThread.Start();
+                
+                ServerStart.Content = "서버 작동중";
+                ServerStart.IsEnabled = false;
+                IsServerStop = false;
+            }
+            
+            else
+            {
+                // ServerThread.Join();
+                ServerStart.Content = "서버 작동중";
+                IsServerStop = true;
+            }
+        }
+
+        // DataGrid에 데이터를 등록함
+        private void ServerManager_DataReceiveEvent(string receiveData)
+        {
+            string time = System.DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss"); // 현재시각
+
+            // 순번, 시간, Client로 부터 들어온 데이터를 등록함
+            index++;
+
+            TCPDataItem addItem = new TCPDataItem(index, time, receiveData);
+
+            // DataList에 등록
+            //foreach (TCPDataItem item in DataList)
+            //{
+            //    App.Current.Dispatcher.Invoke((Action)delegate
+            //    {
+            //        DataList.Add(addItem);
+            //    });
+            //}
+
+            App.Current.Dispatcher.Invoke((Action)delegate
+            {
+                DataList.Add(addItem);
+            });
+        }
+
+        private void Listen()
+        {
+            ServerManager.ServerStart();
+        }
+
+        // 로컬 IP 설정
+        public string GetLocalIP()
+        {
+            string localIP = "Not available, please check your network seetings!";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+    }
+
+    public class TCPserverManager
+    {
+        // 루프백 주소, 컴퓨터 네트워크의 입출력 기능을 시험하기 위해 가상으로 할당한 주소
+        // 이를 활용하면 자기 자신에게 패킷을 보내게 됨
+        public string BindIP { get; set; }
+        public int BindPort { get; set; }
+        public TcpListener server = null; // System.Net.Socket
+        public TcpClient client = null;
+        public IPEndPoint localAddress; // System.Net
+
+
+        // 네트워크 스트림 객체 생성. 데이터의 입출력 처리의 중간자 역할
+        //// 데이터,패킷,비트 등의 일련의 연속성을 갖는 흐름을 의미
+        public NetworkStream stream;
+
+        public delegate void DataReceiveDelegate(string _data);
+        public event DataReceiveDelegate DataReceiveEvent;
+        
+
+        public TCPserverManager()
+        {
+            
+        }
+
+        public void ServerStart()
+        {
+            try
+            {
+                // EndPoint :: 어떠한 소프트웨어나 제품에 최종목적지인 사용자, 
+                // 그 예로는 PC나 노트북, 핸드폰등 유저가 사용하는 devices등
+                localAddress = new IPEndPoint(IPAddress.Parse(BindIP), BindPort);
+
+                server = new TcpListener(localAddress);
+
+                // 서버 시작
+                server.Start();
+
+                while (true)
+                {
+                    client = server.AcceptTcpClient();
+                    stream = client.GetStream();
+                    ManagedStream();
+                }
+            }
+
+            catch (SocketException ex)
+            {
+
+            }
+        }
+
+        public void ServerExit()
+        {
+            if (client != null)
+                client.Close();
+
+            if (server != null)
+                server.Stop();
+        }
+
+        public void ManagedStream()
+        {
+            try
+            {
+                int length;
+                string data = null;
+                byte[] bytes = new byte[256];
+
+                // if ( stream. == null)
+                
+
+                while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // 문자인코딩을 통해 byte형태로 들어오는 것을 string으로 변환
+                    data = Encoding.Default.GetString(bytes, 0, length);
+                    
+                    byte[] msg = Encoding.Default.GetBytes(data);
+
+                    // 데이터 송신
+                    // message 내용, 쓸 위치(offset), message 길이
+                    stream.Write(msg, 0, msg.Length);
+                }
+                // Client로부터 들어온 데이터를 처리하는 TextBlock에 string data 뿌려줌
+                DataReceiveEvent(data);
+
+                stream.Close();
+                // client.Close();
+            }
+            catch (SocketException e)
+            {
+
+            }
+        }
+    }
+
+    public class TCPDataItem
+    {
+        public int Index { get; set; }
+        public string Date { get; set; }
+        public string Data { get; set; }
+
+
+        public TCPDataItem()
+        {
+
+        }
+
+        public TCPDataItem(int _Index, string _Date, string _Data)
+        {
+            Index = _Index;
+            Date = _Date;
+            Data = _Data;
+        }
+    }
+
+}
---
+++ NetworkTCPExample/TCPClient/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCPClient
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // 연결상태 - 처음은 stop이 True
+        public bool IsClientStop = true;
+
+        public TCPclientManager ClientManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+          //  IPtextBox.Text = GetLocalIP();
+
+            ClientManager = new TCPclientManager();
+        }
+
+        // Client Start
+        private void ClientStartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsClientStop)
+            {
+                ClientManager.BindIP = IPtextBox.Text;
+                ClientManager.BindPort = int.Parse(PORTtextBox.Text);
+
+                ClientManager.ServerIP = ServerIPtextBox.Text;
+                ClientManager.ServerPort = int.Parse(ServerPORTtextBox.Text);
+
+                ClientManager.ClientStart();
+
+                IsClientStop = false;
+            }
+
+            else
+            {
+                IsClientStop = true;
+               // ClientManager.ClientStop();
+            }
+        }
+
+        // 보내기
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClientManager.Message = SendMessageTextBox.Text;
+            ClientManager.SendDataStream();
+        }
+
+        // 로컬 IP 설정
+        public string GetLocalIP()
+        {
+            string localIP = "Not available, please check your network seetings!";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+            return localIP;
+        }
+
+    }
+
+    public class TCPclientManager
+    {
+        public string BindIP { get; set; }
+        public int BindPort { get; set; }
+        public string ServerIP { get; set; }
+        public int ServerPort { get; set; }
+
+        // 서버측으로 보낼 메시지
+        public string Message { get; set; }
+
+        public TcpClient client = null; // System.Net.Socket
+        public IPEndPoint clientAddress, serverAddress; // System.Net
+
+        public TCPclientManager()
+        {
+
+        }
+
+        public void ClientStart()
+        {
+            try
+            {
+                // EndPoint :: 어떠한 소프트웨어나 제품에 최종목적지인 사용자, 
+                // 그 예로는 PC나 노트북, 핸드폰등 유저가 사용하는 devices등
+                //if (clientAddress == null || serverAddress == null)
+                //{
+                //    clientAddress = new IPEndPoint(IPAddress.Parse(BindIP), BindPort);
+
+                //    serverAddress = new IPEndPoint(IPAddress.Parse(ServerIP), ServerPort);
+
+                //}
+                //client = new TcpClient(clientAddress);
+
+                //client.Connect(serverAddress);
+                if (BindIP == "" || ServerIP == "") return;
+
+                clientAddress = new IPEndPoint(IPAddress.Parse(BindIP), BindPort);
+
+                serverAddress = new IPEndPoint(IPAddress.Parse(ServerIP), ServerPort);
+
+                // client = new TcpClient(clientAddress);
+                // client.Client.Bind(clientAddress);
+                // client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                // client.Connect(serverAddress);
+            }
+
+            catch (SocketException ex)
+            {
+
+            }
+        }
+
+
+        public void SendDataStream()
+        {
+            try
+            {
+                client = new TcpClient(clientAddress);
+                // client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                client.Connect(serverAddress);
+
+                // TextBox로부터 들어온 보낼 메세지를 TCP 통신을 위한 byte data로 변환
+                byte[] data = System.Text.Encoding.Default.GetBytes(Message);
+
+                // 데이터 송신을 위한 stream 객체를 생성
+                NetworkStream stream =  client.GetStream();
+
+                // stream에 data들을 싣음 (data내용, offset, data길이)
+                stream.Write(data, 0, data.Length);
+
+                data = new byte[256];
+
+                string responseData = "";
+
+                int bytes = stream.Read(data, 0, data.Length);
+
+                responseData = Encoding.Default.GetString(data, 0, bytes);
+
+                // 수신 :: responseData , Textbox에 뿌리던지 말든지
+                stream.Close();
+               // client.Close();
+            }
+            catch (SocketException e)
+            {
+
+            }
+        }
+
+        internal void ClientStop()
+        {
+        }
+    }
+
+}
