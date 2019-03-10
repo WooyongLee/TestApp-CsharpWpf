e6148f1ec6b9659fd3f96c1926a6669e0bbd5034
이우용 <wooyo250@naver.com>
2019-03-11 00:03:50 +0900

    DoubleTick Main

---
+++ DoubleTickSlider/DoubleTickSlider/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Windows;
+
+namespace DoubleTickSlider
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public double Minimum
+        {
+            get { return (double)GetValue(MinimumProperty); }
+            set { SetValue(MinimumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MinimumProperty =
+            DependencyProperty.Register("Minimum", typeof(double), typeof(MainWindow), new UIPropertyMetadata(0d));
+
+        public double LowerValue
+        {
+            get { return (double)GetValue(LowerValueProperty); }
+            set { SetValue(LowerValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty LowerValueProperty =
+            DependencyProperty.Register("LowerValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(0d));
+
+        public double UpperValue
+        {
+            get { return (double)GetValue(UpperValueProperty); }
+            set { SetValue(UpperValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty UpperValueProperty =
+            DependencyProperty.Register("UpperValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(0d));
+
+        public double Maximum
+        {
+            get { return (double)GetValue(MaximumProperty); }
+            set { SetValue(MaximumProperty, value); }
+        }
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register("Maximum", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1d));
+
+    }
+}
