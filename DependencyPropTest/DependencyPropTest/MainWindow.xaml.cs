6dc5ab865c04bfd47423f95360462b96543e0989
이우용 <wooyo250@naver.com>
2019-04-15 21:12:21 +0900

    Dependency Property TestApp

---
+++ DependencyPropTest/DependencyPropTest/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyPropTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty UserProperty = DependencyProperty.Register("TextChange", typeof(String), typeof(MainWindow), new FrameworkPropertyMetadata(new PropertyChangedCallback(OnTextChangePropChanged)));
+
+        // Dependency Property의 CallBack Method
+        private static void OnTextChangePropChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow userNameCtrl = d as MainWindow;
+            string newText = (string)e.NewValue;
+            string oldText = (string)e.OldValue;
+
+            userNameCtrl.OldTextBlock.Text = oldText;
+            userNameCtrl.NewTextBlock.Text = newText;
+        }
+
+        public String UserText
+        {
+            get { return (String)GetValue(UserProperty); }
+            set { SetValue(UserProperty, value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            UserText = textBox1.Text;
+        }
+
+        private void btnSecondPropertyWindowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SecondProp secondWinodow = new SecondProp();
+            secondWinodow.Show();
+        }
+    }
+}
---
+++ DependencyPropTest/DependencyPropTest/SecondProp.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DependencyPropTest
+{
+    /// <summary>
+    /// SecondProp.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SecondProp : Window
+    {
+        public class MyBtn : Button
+        {
+            public static readonly DependencyProperty HiddenTextProperty 
+                = DependencyProperty.Register("HiddenText", typeof(String), typeof(FrameworkElement), new FrameworkPropertyMetadata("default", FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+            public MyBtn() : base()
+            {
+            }
+            public String HiddenText
+            {
+                get { return (String)GetValue(HiddenTextProperty); }
+                set { SetValue(HiddenTextProperty, value); }
+            }
+
+            protected override void OnClick()
+            {
+                base.Content = this.HiddenText;
+                base.OnClick();
+            }
+        }
+        public SecondProp()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyBtn mb = new MyBtn();
+            mb.HiddenText = "hidden test";
+            mb.Width = 200;
+            mb.Height = 100;
+
+            MyGrid.Children.Add((UIElement)mb);
+
+            // MyGrid.Children.Remove((UIElement)mb);
+        }
+    }
+}
