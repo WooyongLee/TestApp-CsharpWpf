93d8f8a6214e93afe5056794930520d0a5bc292f
이우용 <wooyo250@naver.com>
2019-04-16 10:51:42 +0900

    tab control each other Commit

---
+++ TabControlEachOtherUCBinding/TabControlEachOtherUCBinding/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabControlEachOtherUCBinding
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TabViewModel> TabViewModels { get; set; }
+        public TabViewModel SelectedTabViewModel { get; set; }
+
+
+        public int index {get; set;}
+        public ViewModelA VMA = new ViewModelA();
+        public ViewModelB VMB = new ViewModelB();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            VMA.Value = "11111";
+            VMA.DateTime = "11111111@@@@";
+
+            VMB.Value = "222222222";
+            VMB.DateTime = "2222222222#######";
+
+            //TabViewModels = new ObservableCollection<TabViewModel>();
+            //TabViewModels.Add(new ViewModelA { Header = "Tab A" });
+            //TabViewModels.Add(new ViewModelB { Header = "Tab B" });
+
+            //SelectedTabViewModel = TabViewModels[0];
+
+            Tab1Uc.DataContext = VMA; 
+            Tab2Uc.DataContext = VMB; 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            for ( int i = 0 ; i < 5 ; i++)
+            {
+                VMA.ArrayValue[i] = index * i;
+                VMB.ArrayValue[i] = (index * i).ToString();
+            }
+            index++;
+        }
+    }
+}
---
+++ TabControlEachOtherUCBinding/TabControlEachOtherUCBinding/ViewModel.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TabControlEachOtherUCBinding
+{
+    public class TabViewModel : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class ViewModelA : TabViewModel, INotifyPropertyChanged
+    {
+        private string header;
+        private string value;
+        private string datetime;
+
+        // Message #1, Message #2 하는 메세지 인덱스
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                header = value;
+                NotifyPropertyChanged("Header");
+            }
+        }
+
+        // Message Name : Value 쌍을 정의하는 Value
+        public string Value
+        {
+            get { return this.value; }
+            set
+            {
+                this.value = value;
+                NotifyPropertyChanged("Value");
+            }
+        }
+
+        public string DateTime
+        {
+            get { return datetime; }
+            set
+            {
+                this.datetime = value;
+                NotifyPropertyChanged("DateTime");
+            }
+        }
+
+        public ObservableCollection<int> ArrayValue { get; set; }
+
+        public ViewModelA()
+        {
+            ArrayValue = new ObservableCollection<int>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+    public class ViewModelB : TabViewModel, INotifyPropertyChanged
+    {
+        private string header;
+        private string value;
+        private string datetime;
+        private string[] arrayValue = new string[5];
+
+        // Message #1, Message #2 하는 메세지 인덱스
+        public string Header
+        {
+            get { return header; }
+            set
+            {
+                header = value;
+                NotifyPropertyChanged("Header");
+            }
+        }
+
+        // Message Name : Value 쌍을 정의하는 Value
+        public string Value
+        {
+            get { return this.value; }
+            set
+            {
+                this.value = value;
+                NotifyPropertyChanged("Value");
+            }
+        }
+
+        public string DateTime
+        {
+            get { return datetime; }
+            set
+            {
+                this.datetime = value;
+                NotifyPropertyChanged("DateTime");
+            }
+        }
+
+        public string[] ArrayValue
+        {
+            get { return arrayValue; }
+            set
+            {
+                arrayValue = value;
+                NotifyPropertyChanged("ArrayValue");
+            }
+        }
+        public ViewModelB()
+        {
+            ArrayValue = new string[5];
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    public class ViewModelC : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
