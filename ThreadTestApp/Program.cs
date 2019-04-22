bed9abc5b4db78807a9f0a8e64e03f743f9fb3b1
Init wooyo <wooyo250@naver.com>
2019-04-22 21:53:07 +0900

    ThreadTestApp

---
+++ ThreadTestApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTestApp
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().DoTest();
+        }
+
+        void DoTest()
+        {
+            // 새로운 쓰레드에서 Run() 실행
+            Thread t1 = new Thread(new ThreadStart(Run));
+            t1.Start();
+
+            // 메인쓰레드에서 Run() 실행
+            Run();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Thread#{0}: Begin", Thread.CurrentThread.ManagedThreadId);
+            // Do Something
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread#{0}: End", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
