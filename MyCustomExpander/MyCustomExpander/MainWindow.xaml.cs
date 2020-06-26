30d96ec86db3ec9d1ac280696da34761b836bd35
wooyo250 <wooyo5517@gmail.com>
2020-06-26 15:27:46 +0900

    1. CustomExpander 추가

---
+++ MyCustomExpander/MyCustomExpander/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MyCustomExpander
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<VM> VmCollec { get; set; } = new ObservableCollection<VM>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // DataGrid1.ItemsSource = VmCollec;
+
+            VmCollec.Add(new VM() { IsCheck = false, Content = "1" });
+            VmCollec.Add(new VM() { IsCheck = false, Content = "2" });
+            VmCollec.Add(new VM() { IsCheck = true, Content = "3" });
+            VmCollec.Add(new VM() { IsCheck = true, Content = "4" });
+
+            CIBListBox.ItemsSource = VmCollec;
+        }
+    }
+
+    public class VM
+    {
+        public bool IsCheck { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class MultiplyConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = 1.0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] is double)
+                    result *= (double)values[i];
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new Exception("Not implemented");
+        }
+    }
+}
